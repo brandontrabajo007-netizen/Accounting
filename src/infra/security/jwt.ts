bba8d7da9891d4458b33ffcff18b6841d5bc9fa2
Brandon Velandia <brandontrabajo007@gmail.com>
2026-01-06 16:03:39 -0500

    Jws

--- src/infra/security/jwt.ts
+++ src/infra/security/jwt.ts
@@ -8,7 +8,8 @@ export interface ApiTokenPayload extends JwtPayload {
 }
 
 const JWT_SECRET = process.env.JWT_SECRET || 'dev-secret'
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '12h'
+const JWT_EXPIRES_IN = (process.env.JWT_EXPIRES_IN ||
+  '12h') as jwt.SignOptions['expiresIn']
 
 export const generateToken = (payload: ApiTokenPayload) => {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN })
